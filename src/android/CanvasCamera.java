a6439f1e70e849c9176a9311e26046b6fc6c534d
Colin227 <colin9416@gmail.com>
2023-08-23 13:00:30 -0400

    Updated permissions value based on SDK version.

--- src/android/CanvasCamera.java
+++ src/android/CanvasCamera.java
@@ -63,7 +63,7 @@ public class CanvasCamera extends CordovaPlugin implements CanvasCameraInterface
     private static final int SEC_CAMERA_POSITION = 3;
 
     private final static String[] FILENAMES = {"fullsize", "thumbnail"};
-    private final static String[] PERMISSIONS = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private final static String[] PERMISSIONS =  (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) ? new String[] {Manifest.permission.CAMERA} : new String[] {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     protected int mFps;
     protected int mWidth;
